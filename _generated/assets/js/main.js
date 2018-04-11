4a2470d90b5cb6d391ff7a887b88f1afb8d2bbb0
Markus Andersson <make_99@hotmail.com>
2018-04-11 08:29:51 +0200

    setting up enviroment for developing, bootstrap added and some structure

--- _generated/assets/js/main.js
+++ _generated/assets/js/main.js
@@ -1,1 +1,2 @@
 // put your js here :-)
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJtYWluLmpzIl0sInNvdXJjZXNDb250ZW50IjpbIi8vIHB1dCB5b3VyIGpzIGhlcmUgOi0pIl0sImZpbGUiOiJtYWluLmpzIn0=
