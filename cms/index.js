5ed58ac71198000329b19c9bf0b693372e9a5ae4
Markus Andersson <make_99@hotmail.com>
2018-04-11 11:33:15 +0200

    Added some stuff to index page

--- cms/index.js
+++ cms/index.js
@@ -1,3 +1,43 @@
 {
-	
+	first_part:{
+		header: 'ReMix-det licensfria alternativet',
+
+		cards:[
+			{
+				image: '',
+				title: 'ReMix',
+				text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor mollit anim id est laborum.',
+				linkText:'ReMix',
+				link:'/ReMix'
+			},
+			{
+				image: '',
+				title: 'SenseSec',
+				text: 'Lorem ipsum dolor sit amet, consectetur adi',
+				linkText:'SenseSec',
+				link:'/senseSec'
+			}
+		]
+
+	},
+	second_part:{
+		header: 'Utbildning & Workshops',
+
+		cards:[
+			{
+				image: '',
+				title: 'ReMix',
+				text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor mollit anim id est laborum.',
+				linkText:'ReMix',
+				link:'/ReMix'
+			},
+			{
+				image: '',
+				title: 'SenseSec',
+				text: 'Lorem ipsum dolor sit amet, consectetur adi',
+				linkText:'SenseSec',
+				link:'/senseSec'
+			}
+		]
+	}
 }
