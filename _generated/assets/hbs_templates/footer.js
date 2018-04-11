6cf746d4972d533d1d3cdd7b7de146f8f247fd2a
Markus Andersson <make_99@hotmail.com>
2018-04-11 09:27:23 +0200

    added more structure

--- _generated/assets/hbs_templates/footer.js
+++ _generated/assets/hbs_templates/footer.js
@@ -1,3 +1,3 @@
 define(["handlebars"], function(Handlebars) { return Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "";
+    return "<footer>\r\n  \r\n</footer>\r\n";
 },"useData":true}); });
