4487a5715c690de249cef137219feee567d595e0
Markus Andersson <make_99@hotmail.com>
2018-04-11 12:40:55 +0200

    started filling in content

--- cms/index.js
+++ cms/index.js
@@ -1,43 +1,42 @@
-{
-	first_part:{
-		header: 'ReMix-det licensfria alternativet',
-
-		cards:[
+({
+	first_part: {
+		header: 'Här kommer en schysst titel',
+		cards: [
 			{
-				image: '',
-				title: 'ReMix',
-				text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor mollit anim id est laborum.',
-				linkText:'ReMix',
-				link:'/ReMix'
+				header: 'ReMix-det licensfria alternativet',
+				image: 'index/remix-test1.jpg',
+				title: 'Licensfria webbportaler och mobila appar',
+				text: 'Med modern webbteknik och öppna standards digitaliserar vi våra kunders affärsprocesser.  Inga licenskostnader. Driftsäkra, hållbara och uppgraderingsbara. När detta skrivs har lösningar varit i drift i mer än 5 år för affärskritiska tillämpningar.  Tekniskt bygger ReMix på modern webbteknik. API orienterad IT-arkitektur UI/UX HTML5, Angular JS, Bootstrap.',
+				linkText: 'ReMix',
+				link: '/ReMix'
 			},
 			{
-				image: '',
+				header: 'SenseSec',
+				image: 'index/sensesec-test1.jpeg',
 				title: 'SenseSec',
 				text: 'Lorem ipsum dolor sit amet, consectetur adi',
-				linkText:'SenseSec',
-				link:'/senseSec'
+				linkText: 'SenseSec',
+				link: '/senseSec'
 			}
 		]
-
 	},
-	second_part:{
+	second_part: {
 		header: 'Utbildning & Workshops',
-
-		cards:[
+		cards: [
 			{
-				image: '',
-				title: 'ReMix',
-				text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor mollit anim id est laborum.',
-				linkText:'ReMix',
-				link:'/ReMix'
+				image: 'index/workshop-1.jpeg',
+				title: 'Introduktionsutbildning',
+				text: 'En introduktion till licensfria webbtekniker och integration. Tekniker och begrepp som avhandlas är “Single Page Application”, AngularJS, NodeJS, API-er.',
+				linkText: 'Mer info',
+				link: '/senseSec'
 			},
 			{
-				image: '',
-				title: 'SenseSec',
-				text: 'Lorem ipsum dolor sit amet, consectetur adi',
-				linkText:'SenseSec',
-				link:'/senseSec'
+				image: 'index/workshop-2.jpg',
+				title: 'Programmeringscoach och arkitektbollplank',
+				text: 'Vill du ha hjälp ute i din verksamhet kan du hyra en konsult av oss som hjälper er igång och stöttar under hela processen.',
+				linkText: 'Mer info',
+				link: '/ReMix'
 			}
 		]
 	}
-}
+})
