8393d7be442d5fe36e34c20afa95fc36ab313953
Markus Andersson <make_99@hotmail.com>
2018-04-11 10:11:37 +0200

    added new page

--- cms/global/navigation/mainmenu.js
+++ cms/global/navigation/mainmenu.js
@@ -7,6 +7,11 @@
 				new_window: false
 			},
 			{
+				label: 'Produkter',
+				link: '/products',
+				new_window: false
+			},
+			{
 				label: 'Utbildning',
 				link: '/workshop',
 				new_window: false
