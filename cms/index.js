a50147ba6ddabc763dfe79f5830460e79097d372
Roger <rogge_acdc@hotmail.com>
2018-04-11 12:43:46 +0200

    /mockup2: Auto stash before merge of "feature/mockup2" and "origin/develop"

--- cms/index.js
+++ cms/index.js
@@ -1,41 +1,14 @@
 ({
-	first_part: {
-		header: 'Här kommer en schysst titel',
-		cards: [
+	Welcometext: {
+		Heading: [
 			{
-				header: 'ReMix-det licensfria alternativet',
-				image: 'index/remix-test1.jpg',
-				title: 'Licensfria webbportaler och mobila appar',
-				text: 'Med modern webbteknik och öppna standards digitaliserar vi våra kunders affärsprocesser.  Inga licenskostnader. Driftsäkra, hållbara och uppgraderingsbara. När detta skrivs har lösningar varit i drift i mer än 5 år för affärskritiska tillämpningar.  Tekniskt bygger ReMix på modern webbteknik. API orienterad IT-arkitektur UI/UX HTML5, Angular JS, Bootstrap.',
-				linkText: 'ReMix',
-				link: '/ReMix'
+				Label: 'Hej hej'
 			},
 			{
-				header: 'SenseSec',
-				image: 'index/sensesec-test1.jpeg',
-				title: 'SenseSec',
-				text: 'Lorem ipsum dolor sit amet, consectetur adi',
-				linkText: 'SenseSec',
-				link: '/senseSec'
-			}
-		]
-	},
-	second_part: {
-		header: 'Utbildning & Workshops',
-		cards: [
-			{
-				image: 'index/workshop-1.jpeg',
-				title: 'Introduktionsutbildning',
-				text: 'En introduktion till licensfria webbtekniker och integration. Tekniker och begrepp som avhandlas är “Single Page Application”, AngularJS, NodeJS, API-er.',
-				linkText: 'Mer info',
-				link: '/senseSec'
+				Label: 'kalops'
 			},
 			{
-				image: 'index/workshop-2.jpg',
-				title: 'Programmeringscoach och arkitektbollplank',
-				text: 'Vill du ha hjälp ute i din verksamhet kan du hyra en konsult av oss som hjälper er igång och stöttar under hela processen.',
-				linkText: 'Mer info',
-				link: '/ReMix'
+				Label: 'ärtsoppa'
 			}
 		]
 	}
