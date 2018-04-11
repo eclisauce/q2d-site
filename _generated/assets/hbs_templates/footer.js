b5e89f290eb18e19a923e38480d3521ad4e8b22a
Markus Andersson <make_99@hotmail.com>
2018-04-11 08:19:02 +0200

    initial

--- _generated/assets/hbs_templates/footer.js
+++ _generated/assets/hbs_templates/footer.js
@@ -1,3 +1,3 @@
 define(["handlebars"], function(Handlebars) { return Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<section class=\"screen half\">\n	<div class=\"center_block\">\n		<p>For support visit enduro.js on <a href=\"https://github.com/Gottwik/Enduro\">github</a></p>\n	</div>\n</section>";
+    return "<section class=\"screen half\">\r\n	<div class=\"center_block\">\r\n		<p>For support visit enduro.js on <a href=\"https://github.com/Gottwik/Enduro\">github</a></p>\r\n	</div>\r\n</section>";
 },"useData":true}); });
