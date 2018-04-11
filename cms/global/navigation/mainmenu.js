7f8b607de770c148b03db415bfb22ebab99049d1
Markus Andersson <make_99@hotmail.com>
2018-04-11 09:22:15 +0200

    added header to global

---
+++ cms/global/navigation/mainmenu.js
@@ -0,0 +1,21 @@
+({
+	mainmenu: {
+		items: [
+			{
+				label: 'Home',
+				link: '/',
+				new_window: false
+			},
+			{
+				label: 'Themes',
+				link: '/themes',
+				new_window: false
+			},
+			{
+				label: 'Documentation',
+				link: '/docs',
+				new_window: false
+			}
+		]
+	}
+})
