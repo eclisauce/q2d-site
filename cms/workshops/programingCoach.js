b4c8059dd429f55f6cebbcb83a4b425507803472
Markus Andersson <make_99@hotmail.com>
2018-04-12 08:51:09 +0200

    next page completed

--- cms/workshops/programingCoach.js
+++ cms/workshops/programingCoach.js
@@ -1,3 +1,40 @@
-{
-
-}
+({
+	programingCoach: {
+		header: 'Programmerings coaching och arkitektbollplank',
+		jumbotronText: 'Två tjänster som vi erbjuder innebär utbildning på plats; programmerings coaching och arkitektbollplank.',
+		items: [
+			{
+				heading: '',
+				texts: [
+					{
+						text: 'Upptäck möjligheterna och potentialen med webbteknik i modern applikationsutveckling. Se hur detta kan användas i molnlösningar.'
+					}
+				]
+			},
+			{
+				heading: '',
+				texts: [
+					{
+						text: 'Ett bollblank till arkitekten ger tips och råd hur den nuvarande arkitekturen steg för steg kan vridas mot en API-orienterad arkitektur. Vi kan med exempel från de projekt vi har arbetat med, visa på nya sätt att lösa era utmaningar.'
+					}
+				]
+			},
+			{
+				heading: '',
+				texts: [
+					{
+						text: 'Vi erbjuder också supportpaket i olika nivåer där vi tar över hela eller delar av en IT-lösning eller projekt. För företag och organisationer med egna utvecklare finns det möjlighet att teckna ett samarbetsavtal med fokus på support. Detta kan gälla konkreta problemlösningar som ligger inom vårt teknikområde.'
+					}
+				]
+			},
+			{
+				heading: '',
+				texts: [
+					{
+						text: 'Supporten utförs antingen som hjälp till självhjälp med coach eller att vårt team helt löser en konkret uppgift. Supportavtalet som tecknas reglerar omfattning av uppgifter och inom vilken tid vi ska återkomma med lösning.'
+					}
+				]
+			}
+		]
+	}
+})
