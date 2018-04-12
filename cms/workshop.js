aaa758a58966c4e5263f847dbd07088f6c0b0e0d
Markus Andersson <make_99@hotmail.com>
2018-04-12 13:01:23 +0200

    added content to workshop page, might need more styling

--- cms/workshop.js
+++ cms/workshop.js
@@ -1,3 +1,39 @@
-{
-  
-}
+({
+  workshop: {
+		heading: 'Utbildningar och Workshops vi erbjuder',
+		workshopJumbotron: [
+			{
+				title: 'Introduktion till programmering',
+				workshopJumbotronId: 'workshopJumbotronIntroduction',
+				textOne: 'En introduktion till licensfria webbtekniker och integration.',
+				textTwo: 'Tekniker och begrepp som avhandlas är “Single Page Application”, AngularJS, NodeJS, API-er.',
+				linkText: 'Läs mer',
+				link: '/workshops/introduction'
+			},
+      {
+				title: 'Programmering med coach och arkitektbollplank',
+				workshopJumbotronId: 'workshopJumbotronProgramingCoach',
+				textOne: 'Vill du ha hjälp ute i din verksamhet kan du hyra en konsult av oss som hjälper er igång och stöttar under hela processen.',
+				textTwo: '',
+				linkText: 'Läs mer',
+				link: '/workshops/programingCoach'
+			},
+      {
+				title: 'Heldag Workshop',
+				workshopJumbotronId: 'workshopJumbotronFull',
+				textOne: 'Modern webbteknik och integration',
+				textTwo: '',
+				linkText: 'Läs mer',
+				link: '/workshops/workshopFull'
+			},
+      {
+				title: 'Halvdag Workshop',
+				workshopJumbotronId: 'workshopJumbotronHalf',
+				textOne: 'Modern webbteknik och integration',
+				textTwo: '',
+				linkText: 'Läs mer',
+				link: '/workshops/workshopHalf'
+			}
+		]
+	}
+})
