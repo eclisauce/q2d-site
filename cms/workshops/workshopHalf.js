8c93c19543a8efa3ede177c699b69234f1d813ec
Markus Andersson <make_99@hotmail.com>
2018-04-12 09:26:09 +0200

    last two pages completed for the workshops
    Might need some more styling and new fonts

--- cms/workshops/workshopHalf.js
+++ cms/workshops/workshopHalf.js
@@ -1,3 +1,49 @@
-{
-
-}
+({
+	workshopHalf: {
+		header: 'Workshop Halvdag workshop (ca 4 Timmar)',
+		memberNumber: 'Max 8 deltagare',
+		price: 'Pris: 12.000 SEK',
+		items: [
+			{
+				heading: '',
+				texts: [
+					{
+						text: 'Vi håller en workshop under en halvdag.pptäck möjligheterna och potentialen med webbteknik i modern applikationsutveckling. Se hur detta kan användas i molnlösningar.'
+					}
+				]
+			},
+			{
+				heading: '',
+				texts: [
+					{
+						text: 'Under den här sessionen kommer vi att presentera de olika möjligheterna med modern webbteknik och integration. Som deltagare får du en förståelse för hur moderna applikationer byggs upp i flera lager med möjlighet till koppling eller drift i molnet. Arkitektur och teknik matchas sedan med 1 konkret behov från verksamheten, som sedan används som tema för workshopen.'
+					}
+				]
+			},
+			{
+				heading: '',
+				texts: [
+					{
+						text: 'Målet för deltagarna är att få en introduktion till ny teknik och nya arbetsmetoder som öppnar helt nya möjligheter och snabba resultat. Inga särskilda kunskaper i programmering krävs.'
+					}
+				]
+			},
+			{
+				heading: '',
+				texts: [
+					{
+						text: 'Användargränssnittet bygger på “Single Page Application” (SPA) och vi använder främst AngularJS för att strukturera webbapplikationen.'
+					}
+				]
+			},
+			{
+				heading: '',
+				texts: [
+					{
+						text: 'För backend sätter vi upp en enkel webbserver i NodeJS där vi kan skapa exakt de API som behövs för att modellera fram användarupplevelsen.'
+					}
+				]
+			}
+		]
+	}
+})
