46dbd46ff24f5c41aa1d022cb3cf6a8f1a622d87
Markus Andersson <make_99@hotmail.com>
2018-04-16 14:56:30 +0200

    Added content to ReMix page and made it work

--- cms/products/ReMix.js
+++ cms/products/ReMix.js
@@ -1,3 +1,136 @@
-{
-  
-}
+({
+  ReMix: {
+    sectionOne: {
+      headingJumboTron: 'ReMix',
+      image: 'index/remix-test1.jpg',
+      textJumboTron: 'ReMix – en innovativ lösning som gör ditt företag flexibelt',
+      textOne: 'Snabba resultat, låga kostnader, modern teknik och hög säkerhet.',
+      textTwo: 'Med Remix tar du fram det bästa ur dina existerande IT-system och skapar snabbt nya lösningar. ReMix bygger helt på öppna standards.',
+      accordion: [{
+          id: 'headingOne',
+          target: 'collapseOne',
+          header: 'Remix för SAP',
+          title: 'ReMix för SAP',
+          titleh3: 'En innovativ lösning för företag som kör SAP',
+          textOne: 'Snabba resultat, låga kostnader, modern teknik och hög säkerhet.',
+          textTwo: 'Med Remix för SAP tar du fram det bästa ur dina existerande IT-system och skapar snabbt nya lösningar. ReMix för SAP bygger helt på öppna standards och vi följer SAPs standards för integration.',
+          titleh4: 'Målgrupp',
+          textThree: 'Företag som vill utveckla sin verksamhet med moderna webb- och mobillösningar som är integrerade med SAP och andra system.',
+          textFour: 'Vad ingår i Remix för SAP?',
+          list: [{
+              listText: 'En webbportal mot SAP som är mobilanpassad.'
+            },
+            {
+              listText: 'En mobil portal mot SAP'
+            },
+            {
+              listText: 'En integrationsdel där du kopplar ihop dina system'
+            },
+            {
+              listText: 'Färdiga mallar samt kopplingar mot SAP, Microsoft Nav och andra system'
+            }
+          ],
+          titleh4Two: 'Hur snabbt kan jag skapa exempelvis en kundportal med Remix för SAP?',
+          textFive: 'Räkna med 8 veckor för att få upp en kundportal i drift fullt integrerad med SAP.'
+        },
+        {
+          id: 'headingTwo',
+          target: 'collapseTwo',
+          header: 'We make SAP easy to use',
+          title: '',
+          titleh3: 'ReMix gör det enkelt för användarna att arbeta med SAP',
+          textOne: 'q2d arbetar med att göra SAP-data tillgängligt i andra applikationer och genom andra användargränssnitt än SAP GUI. Ofta handlar det om att göra SAP mer lättanvänt.',
+          titleh4: 'Förenkla för ”sällananvändare” av SAP',
+          textThree: 'Med ReMix skapar du snabbt och till en låg kostnad användarvänliga gränssnitt.',
+          titleh4Two: 'SAP Netweaver Gateway och SAP Hana',
+          textFive: 'ReMix mobil och ReMix webb kan ansluta direkt mot SAP via Netweaver Gateway och SAP Hana.'
+        }
+      ]
+    },
+    sectionTwo:{
+      heading: 'Målgrupp',
+      textOne: 'Företag som vill utveckla sin verksamhet med moderna webb- och mobillösningar som är integrerade med andra system.',
+      textTwo: 'Vad ingår i Remix?',
+      list: [{
+          listText: 'En webbportal som är mobilanpassad (se nedan för exempel)'
+        },
+        {
+          listText: 'En mobil portal (se nedan för exempel)'
+        },
+        {
+          listText: 'En integrationsdel där du kopplar ihop dina system'
+        },
+        {
+          listText: 'Färdiga mallar samt kopplingar mot exempelvis SAP och Microsoft Nav'
+        }
+      ],
+      headingTwo: 'Hur snabbt kan jag skapa exempelvis en kundportal med Remix?',
+      textThree: 'Räkna med 8 veckor för att få upp en kundportal fullt integrerad med ditt affärssystem.',
+      webbportal:{
+        btnTitle: 'Webbportal',
+        title: 'ReMix Webb',
+        image: 'products/remix_webb.jpg',
+        textOne: 'q2d förenklar och effektiviserar verksamheter med hjälp av webblösningar',
+        textTwo: 'Här är några exempel på olika typer av webblösningar q2d varit involverade i:',
+        list: [{
+            listText: 'Intern säljportal. Portalen sammanställer information ifrån flera olika system (CRM, ERP, BI) och informationen presenteras i ett användarvänligt gränssnitt.'
+          },
+          {
+            listText: 'Mobil kundportal. B2C. Privatpersoner kan i sin mobiltelefon teckna avtal samt följa upp sitt avtal med företaget. Man når lösningen från appar för Apple, Android och Windows'
+          },
+          {
+            listText: 'Kundportal. e-handel B2B. Kunderna kan se lagersaldon och lägga orders. De ser sina kundunika artiklar, avtal och priser, tidigare betällningar m m.'
+          },
+          {
+            listText: 'Kundportal för maskintillbehör: e-handel B2B. Kunderna kan se sina maskiner och de tillbehör som passar till dessa maskiner. De kan se lagersaldon lägga order m m.'
+          },
+          {
+            listText: 'Sälj- och offerthanteringssystem. En global CRM lösning där egna säljare och fristående agenter loggar in och skapar offerter. Produkterna är komplexa och unika och offerten skapas i en produktkonfigurator.'
+          },
+          {
+            listText: 'Kundserviceportal. Alla Sveriges medborgare kan själv söka och hämta dokument och blanketter (self service) samt underhålla personliga data. Inloggning sker via bank ID.'
+          },
+          {
+            listText: 'Intranät. Globalt intranät med dokumenthantering och samarbetsprogram.'
+          },
+          {
+            listText: 'Logistikportal. Logistikansvarig får i en portal överblick över logistikflödet. Data ifrån leverantörer och transportörer är integrerade i logistikportalen.'
+          },
+          {
+            listText: 'Kundportal för orderläggning och self service. e-handel B2B.'
+          },
+          {
+            listText: 'Samarbetsportal för musikintresserade. Hantering av noter on-line. Integration med smartphones och iPad.'
+          }
+        ]
+
+      },
+      mobilportal:{
+        btnTitle: 'Mobil portal',
+        sectionOne:{
+          title: 'ReMix Mobil',
+          textOne: 'q2d gör din verksamhet mobil',
+          textTwo: 'Cross platform design. ReMix mobil fungerar på alla plattformar och alla storlekar på skärmar, plattor och mobiler.'
+        },
+        sectionTwo:{
+          title: 'ReMix Mobila appar',
+          heading: 'För företag som vill vara smarta',
+          textOne: 'ReMix webb och ReMix mobil är byggd med samma teknik, webbteknik. Vill man ha lösningar i form av en app på olika plattformar (Apple, Android, Windows m fl) hanterars även detta i ReMix.',
+          textTwo: 'Tekniken som ReMix bygger på gör att större delen av utvecklingen för att bygga en app som ska fungera på olika plattformar är gemensam.',
+          textThree: 'För dig som kund blir det billigare och det går snabbare att få ut dina appar.',
+          image: 'products/mobil_app.png'
+
+        },
+        sectionThree:{
+          title: 'ReMix Mobila Tilläggstjänster',
+          textOne: 'I ReMix mobil finns möjlighet att ansluta diverse tilläggs tjänster till din lösning.',
+          textTwo: 'Exempelvis push, olika positioneringstjänster, tidsbegränsade meddelanden, meddelanden mot visa geografiska områden, diverse statistik på hur användaren interagerar mot appen.',
+          textThree: 'Mycket av tilläggstjänsterna är utveckalde för marknadsföring men kan naturligtvis användas i vilken tillämpning som helst.',
+          image: 'products/it_partner.png'
+
+        }
+
+      }
+    }
+  }
+})
