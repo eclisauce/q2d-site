b1ca6464f1f0cd63a97d11a7055bde7f567dde9a
Markus Andersson <make_99@hotmail.com>
2018-04-17 09:36:06 +0200

    Font size fixes for mobile

--- cms/product.js
+++ cms/product.js
@@ -1,34 +1,35 @@
 ({
-  product: {
-    header: 'Produkter från q2d',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    productJumbotron: [{
-        title: 'ReMix',
-        productJumbotronId: 'productJumbotronReMix',
-        image: 'workshop/introductionJumbotron-img.jpeg',
-        textOne: 'En introduktion till licensfria webbtekniker och integration.',
-        textTwo: '',
-        linkText: 'Läs mer',
-        link: '/products/ReMix'
-      },
-      {
-        title: 'Licensfri integration',
-        productJumbotronId: 'productJumbotronIntegration',
-        image: 'workshop/introductionJumbotron-img.jpeg',
-        textOne: 'En introduktion till licensfria webbtekniker och integration.',
-        textTwo: '',
-        linkText: 'Läs mer',
-        link: '/products/integration'
-      },
-      {
-        title: 'SenseSec',
-        productJumbotronId: 'productJumbotronSenseSec',
-        image: 'workshop/introductionJumbotron-img.jpeg',
-        textOne: 'En introduktion till licensfria webbtekniker och integration.',
-        textTwo: '',
-        linkText: 'Läs mer',
-        link: '/products/senseSec'
-      }
-    ]
-  }
+	product: {
+		header: 'Produkter från q2d',
+		text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+		productJumbotron: [
+			{
+				title: 'ReMix',
+				productJumbotronId: 'productJumbotronReMix',
+				image: 'index/remix-test1.jpg',
+				textOne: 'Licensfria webbportaler och mobila appar',
+				textTwo: '',
+				linkText: 'Läs mer',
+				link: '/products/ReMix'
+			},
+			{
+				title: 'Licensfri integration',
+				productJumbotronId: 'productJumbotronIntegration',
+				image: 'index/integration-test1.jpeg',
+				textOne: 'q2d kan integration och vår lösning har använts i affärskritiska applikationer i mer än 5 år.',
+				textTwo: '',
+				linkText: 'Läs mer',
+				link: '/products/integration'
+			},
+			{
+				title: 'SenseSec',
+				productJumbotronId: 'productJumbotronSenseSec',
+				image: 'index/sensesec-test1.jpeg',
+				textOne: 'En introduktion till licensfria webbtekniker och integration.',
+				textTwo: '',
+				linkText: 'Läs mer',
+				link: '/products/senseSec'
+			}
+		]
+	}
 })
