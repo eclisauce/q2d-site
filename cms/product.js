c572e45e336f1fccb08e127bb428230a269e072c
Markus Andersson <make_99@hotmail.com>
2018-04-17 08:30:46 +0200

    image fix and added content to product pages

--- cms/product.js
+++ cms/product.js
@@ -1,3 +1,34 @@
-{
-
-}
+({
+  product: {
+    header: 'Produkter från q2d',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    productJumbotron: [{
+        title: 'ReMix',
+        productJumbotronId: 'productJumbotronReMix',
+        image: 'workshop/introductionJumbotron-img.jpeg',
+        textOne: 'En introduktion till licensfria webbtekniker och integration.',
+        textTwo: '',
+        linkText: 'Läs mer',
+        link: '/products/ReMix'
+      },
+      {
+        title: 'Licensfri integration',
+        productJumbotronId: 'productJumbotronIntegration',
+        image: 'workshop/introductionJumbotron-img.jpeg',
+        textOne: 'En introduktion till licensfria webbtekniker och integration.',
+        textTwo: '',
+        linkText: 'Läs mer',
+        link: '/products/integration'
+      },
+      {
+        title: 'SenseSec',
+        productJumbotronId: 'productJumbotronSenseSec',
+        image: 'workshop/introductionJumbotron-img.jpeg',
+        textOne: 'En introduktion till licensfria webbtekniker och integration.',
+        textTwo: '',
+        linkText: 'Läs mer',
+        link: '/products/senseSec'
+      }
+    ]
+  }
+})
