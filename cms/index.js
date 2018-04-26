73f5c5e9196c539eb3b84b206abc02a580203b35
Markus Andersson <make_99@hotmail.com>
2018-04-26 11:06:54 +0200

    Rework on startpage

--- cms/index.js
+++ cms/index.js
@@ -1,40 +1,15 @@
 ({
-	topImage:{
+	topImage: {
 		image: 'bg-first.jpg',
 		title: 'Licensfria webbportaler och mobila appar',
 		subText: 'Snabbt, användarvänligt, säkert och kostnadseffektivt'
 	},
-	firstPart:{
-		title:'q2d Solutions – Din IT-partner',
-		text:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-			cards: [{
-	        header: 'ReMix',
-	        image: 'index/remix-test1.jpg',
-	        title: 'Licensfria webbportaler och mobila appar',
-	        text: 'Med modern webbteknik och öppna standards digitaliserar vi våra kunders affärsprocesser.  Inga licenskostnader. Driftsäkra, hållbara och uppgraderingsbara. När detta skrivs har lösningar varit i drift i mer än 5 år för affärskritiska tillämpningar.  Tekniskt bygger ReMix på modern webbteknik. API orienterad IT-arkitektur UI/UX HTML5, Angular JS, Bootstrap.',
-	        linkText: 'Mer info',
-	        link: '/products/ReMix'
-	      },
-	      {
-	        header: 'Integration',
-	        image: 'index/integration-test1.jpeg',
-	        title: 'Licensfri Integration',
-	        text: 'q2d kan integration och vår lösning har använts i affärskritiska applikationer i mer än 5 år.  Driftsäker och mycket ekonomisk.  Vi integrerar mot alla typer av affärssystem och andra system. För flera affärsystem finns färdiga mallar. Exempelvis för SAP.',
-	        linkText: 'Mer info',
-	        link: '/products/integration'
-	      },
-	      {
-	        header: 'SenseSec',
-	        image: 'index/sensesec-test1.jpeg',
-	        title: 'SenseSec',
-	        text: 'Lorem ipsum dolor sit amet, consectetur adi',
-	        linkText: 'Mer info',
-	        link: '/products/senseSec'
-	      }
-	    ]
+	firstPart: {
+		title: 'q2d Solutions – Din IT-partner',
+		text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 	},
-
-	secondPart:{
-
+	secondPart: {},
+	meta: {
+		last_edited: 1524722086
 	}
 })
