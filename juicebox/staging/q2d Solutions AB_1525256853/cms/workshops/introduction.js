98a3fba8803c60b139271e38497b2d893596c419
Markus Andersson <make_99@hotmail.com>
2018-05-02 12:30:35 +0200

    testing with changes from admin view to get it on github

---
+++ juicebox/staging/q2d Solutions AB_1525256853/cms/workshops/introduction.js
@@ -0,0 +1,98 @@
+({
+  introduction: {
+    title: 'Gör er redo för molnet',
+    image: 'workshop/introductionJumbotron-img.jpeg',
+    headerOne: 'Introduktionsutbildning',
+    headerTwo: '2 tim stöd efter kursen ingår',
+    textOne: 'Lär er de nya teknikerna som gör det möjligt att kraftigt kapa tider och kostnader för att ta fram och underhålla IT-lösningar.',
+    textTwo: 'Halvdag (ca 4 timmar) den 17e maj 2017. Start kl 13.00 Ideon, Lund',
+    items: [{
+        heading: 'Syfte',
+        texts: [{
+          text: 'Upptäck möjligheterna och potentialen med webbteknik i modern applikationsutveckling. Se hur detta kan användas i molnlösningar.'
+        }]
+      },
+      {
+        heading: 'Målgrupp',
+        texts: [{
+            text: 'Utvecklare som är nya inom webbteknik'
+          },
+          {
+            text: 'Projektledare inom IT-utveckling'
+          },
+          {
+            text: 'Utvecklare som är nya inom webbteknik'
+          }
+        ]
+      },
+      {
+        heading: 'Mål',
+        texts: [{
+            text: 'Få en introduktion till arkitekturen och verktygslådan för modern applikationsutveckling.'
+          },
+          {
+            text: 'Få prova enklare praktiska övningar, som sedan på egen hand kan göras mer avancerade.'
+          },
+          {
+            text: 'Konkret förstå hur man ska gå vidare och utvärdera tekniken i sin egen organisation.'
+          }
+        ]
+      },
+      {
+        heading: 'Förkunskaper',
+        texts: [{
+            text: 'Inga särskilda kunskaper i programmering behövs.'
+          },
+          {
+            text: 'Fördel om erfarenhet av applikationsutveckling finns.'
+          }
+        ]
+      },
+      {
+        heading: 'Kunskaper',
+        texts: [{
+            text: 'HTML5 och den moderna webben'
+          },
+          {
+            text: 'Appar och Webbappar'
+          },
+          {
+            text: 'JavaScript'
+          },
+          {
+            text: 'AngularJS, Bootstrap och kort om andra ramverk'
+          },
+          {
+            text: 'JSON och RESTful API'
+          },
+          {
+            text: 'Backendlösningar för den moderna webben'
+          },
+          {
+            text: 'Använda molnet istället för egna servrar'
+          },
+          {
+            text: 'Webben och framtiden'
+          },
+          {
+            text: 'Molnet och webben skapar nytt arbetssätt'
+          }
+        ]
+      }
+    ]
+  },
+  educator: {
+    heading: 'Utbildare',
+    name: 'Andreas Ohlsson',
+    image: 'workshop/andreas_ohlsson.jpg',
+    infoText: 'Professionell utbildare inom IT och ekonomi i nästan 10 år. Arbetat med IT, affärssystem och systemutveckling i snart 20 år. Lösningsarkitekt och evangelist för modern webbteknik inom applikationsutveckling.',
+    price: 'Pris: 4.000 SEK / deltagare',
+    readerInfo: 'Det är möjligt att medtaga egen dator. Ett begränsat antal lånedatorer finns också.'
+  },
+  application: {
+    heading: 'Anmälan',
+    latestDate: 'Senaste anmälningsdag 2017-05-02',
+    applicationPhone: 'Anmälan på telefon: 046-2862410',
+    applicationEmail: 'Anmälan på e-post till info@q2d.se eller klicka på länken nedan.'
+  }
+})
