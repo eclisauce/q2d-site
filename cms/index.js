975f8172421773b7884ba72b5edaff1cdefebc59
Markus Andersson <make_99@hotmail.com>
2018-05-07 14:23:47 +0200

    rework on startpage

--- cms/index.js
+++ cms/index.js
@@ -1,15 +1,18 @@
 ({
 	topImage: {
-		image: 'bg-second.jpeg',
-		title: 'Licensfria webbportaler och mobila appar',
-		subText: 'Snabbt, användarvänligt, säkert och kostnadseffektivt'
+	  image: 'bg-third.jpeg',
+	  title: 'Licensfria webbportaler och mobila appar',
+	  subText: 'Snabbt, användarvänligt, säkert och kostnadseffektivt'
 	},
 	firstPart: {
-		title: 'q2d Solutions – Din IT-partner',
-		text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+	  title: 'q2d Solutions – Din IT-partner',
+	  text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 	},
-	secondPart: {},
-	meta: {
-		last_edited: 1524722086
+	secondPart: {
+
+	},
+	thirdPart: {
+
 	}
+
 })
