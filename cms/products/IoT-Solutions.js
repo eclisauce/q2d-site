fa97eba3c23be25b519bf701e61d1c6978c69533
Markus Andersson <make_99@hotmail.com>
2018-05-14 13:59:34 +0200

    fixing some files

--- cms/products/IoT-Solutions.js
+++ cms/products/IoT-Solutions.js
@@ -1,54 +1,52 @@
 ({
-	iotSolutions: {
-		heading: 'IoT lösningar baserat på LoRaWAN',
-		image: '',
-		textOne: 'q2d levererar IoT lösningar baserade på LoRaWAN tekniken.',
+  iotSolutions: {
+    heading: 'IoT lösningar baserat på LoRaWAN',
+    image: '',
+    textOne: 'q2d levererar IoT lösningar baserade på LoRaWAN tekniken.',
     subHeaderOne: 'Infrastruktur:',
-		textTwo: 'LoRa sensorer och gateways kommer ifrån Elsys. Läs mer om produkterna du kan välja bland på',
-		textThree: 'SenseSec är vår programvara som hanterar sensorerna och infrastrukturen',
+    textTwo: 'LoRa sensorer och gateways kommer ifrån Elsys. Läs mer om produkterna du kan välja bland på',
+    textThree: 'SenseSec är vår programvara som hanterar sensorerna och infrastrukturen',
     subHeaderTwo: 'Prototypverkstad:',
     textFour: 'Vi hjälper er att testa IoT lösningar i din verksamhet.',
     linkOne: 'https://www.elsys.se/en/',
     linkOneText: 'Elsys hemsida',
     linkTwo: '',
     linkTwoText: 'SenseSec'
-	},
-	LoRaWAN: {
-		btnText: 'Läs mer om LoRaWAN…',
-		title: 'LoRaWAN',
-		textOne: 'LoRaWAN är en öppen global standard för Low Power Wide Area Networks (LPWAN). Med LoRaWAN kopplas batteridrivna enheter upp på internet.',
-		subHeaderOne: 'LoRaWAN kännetecknas av:',
-		listOne: [
-			{
-				listText: 'lång räckvidd'
-			},
-			{
-				listText: 'liten strömförbrukning'
-			},
-			{
-				listText: 'små datamängder'
-			},
-			{
-				listText: 'stor förmåga att ”tränga igenom” med signaler till svåråtkomliga utrymmen'
-			},
-			{
-				listText: 'lågt pris'
-			}
-		],
-		textTwo: 'Enheterna drivs med batteri. Batterierna har en drifttid på upp till 10år.',
-		textThree: 'Signalerna har en räckvidd på upp till 15 km.',
-		textFour: 'Användningsområdena är många då man nu kan mäta på svåråtkomliga ställen utan behov av el, internet eller mobil täckning där sensorerna är placerade. Slipp dyr kabeldragning eller dyr utbyggnad av WiFi. Några exempel:',
-		listTwo: [
-			{
-				listText: 'Fastigheter, fabriker, lagerlokaler, parkeringshus. Placera ut batteridrivna sensorer för bättre styrning av underhåll, övervakning av temperatur och fuktighet, rörelser, vattenläckage, öppning och stängning av portar, luckor, brunnar m m.'
-			},
-			{
-				listText: 'Jordbruk för bättre kontroll av bevattning, gödning och besprutning.'
-			},
-			{
-				listText: 'Transporter för bättre klimatkontroll av exemplvis frukt, kött och andra färskvaror.'
-			}
-		],
+  },
+  LoRaWAN: {
+    btnText: 'Läs mer om LoRaWAN…',
+    title: 'LoRaWAN',
+    textOne: 'LoRaWAN är en öppen global standard för Low Power Wide Area Networks (LPWAN). Med LoRaWAN kopplas batteridrivna enheter upp på internet.',
+    subHeaderOne: 'LoRaWAN kännetecknas av:',
+    listOne: [{
+        listText: 'lång räckvidd'
+      },
+      {
+        listText: 'liten strömförbrukning'
+      },
+      {
+        listText: 'små datamängder'
+      },
+      {
+        listText: 'stor förmåga att ”tränga igenom” med signaler till svåråtkomliga utrymmen'
+      },
+      {
+        listText: 'lågt pris'
+      }
+    ],
+    textTwo: 'Enheterna drivs med batteri. Batterierna har en drifttid på upp till 10år.',
+    textThree: 'Signalerna har en räckvidd på upp till 15 km.',
+    textFour: 'Användningsområdena är många då man nu kan mäta på svåråtkomliga ställen utan behov av el, internet eller mobil täckning där sensorerna är placerade. Slipp dyr kabeldragning eller dyr utbyggnad av WiFi. Några exempel:',
+    listTwo: [{
+        listText: 'Fastigheter, fabriker, lagerlokaler, parkeringshus. Placera ut batteridrivna sensorer för bättre styrning av underhåll, övervakning av temperatur och fuktighet, rörelser, vattenläckage, öppning och stängning av portar, luckor, brunnar m m.'
+      },
+      {
+        listText: 'Jordbruk för bättre kontroll av bevattning, gödning och besprutning.'
+      },
+      {
+        listText: 'Transporter för bättre klimatkontroll av exemplvis frukt, kött och andra färskvaror.'
+      }
+    ],
 
-	}
+  }
 })
