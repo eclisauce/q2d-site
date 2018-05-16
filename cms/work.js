41bf0dee504cc10d03171d9997d32ba2cdc27e14
Markus Andersson <make_99@hotmail.com>
2018-05-16 08:48:06 +0200

    Started filling in the work page and starting to make it dynamicly

---
+++ cms/work.js
@@ -0,0 +1,77 @@
+({
+  avalibleWork: {
+  work: [{
+    firstPart: {
+      title: 'Jobb',
+      workTitle: 'Exjobb: Lightweight Security Testing of Software Products',
+      image: 'sensesec-test1.jpeg',
+      items: [{
+        texts: [{
+            text: 'Thesis description Common Criteria is an ISO/IEC standard for computer security certification. With a total of a 1080 pages this standard is very cumbersome to handle and the cost of certification is prohibitive for small and medium sized enterprises (SMEs). Thus a lot of low-hanging fruits for improving the security of software products are available that cannot be reaped by SMEs due to lack of expertise and cost explosion. The goal of the proposed thesis is to design a simple set of tests and tools that can improve the security of a software product with low overhead. The tests and tools will be tested on a software product by our cooperation partner Q2D Solutions. RISE SICS will provide background information and the necessary guidance during the course of the thesis. '
+          },
+          {
+            text: ''
+          },
+          {
+            text: ''
+          }
+        ]
+      }],
+      list: [{
+        heading: 'The tasks of the Masters student for this thesis are:',
+        texts: [{
+            text: 'Conduct a high level study of Common Criteria and related security certification methods',
+          },
+          {
+            text: 'Find a set of most common vulnerabilities in software products',
+          },
+          {
+            text: 'Design a set of tests to detect those vulnerabilities',
+          },
+          {
+            text: 'Apply the tests to the Q2D product',
+          },
+          {
+            text: 'Write a short manual that enables non-security experts to apply these tests and resolve the security issues found',
+          },
+          {
+            text: 'Document their findings and designs in the thesis manuscript',
+          }
+        ]
+      }]
+    },
+
+    secondPart: {
+      heading: 'Competence',
+      items: [{
+        texts: [{
+            text: 'We are looking for a team of two bright and motivated MSc students who have fulfilled the course requirements, and who have attended some security courses. Good English writing skills are also required. Pedagogical competence is a plus. Applications should include a brief personal statement, CV, and a list of grades. In the application, make sure to mention previous activities or other projects that you consider relevant for the position. Candidates are encouraged to send in their application as soon as possible. Suitable applicants will be interviewed as applications are received. '
+          },
+          {
+            text: ''
+          },
+          {
+            text: ''
+          }
+        ]
+      }]
+    },
+
+    thirdPart: {
+      headingOne: 'Start time:',
+      textOne: 'As soon as possible',
+      headingTwo: 'Location:',
+      textTwo: 'RISE SICS AB & Q2d Solutions AB, Lund (Ideon Beta 2)',
+    },
+
+    fourthPart: {
+      companyOne: 'About RISE SICS',
+      companyOneInfo: 'RISE is non-profit research institute owned by the Swedish government. The mission of RISE SICS is to contribute to the competitive strength of Swedish industry by conducting research in strategic areas of computer science. The Security Laboratory at RISE SICS works on topics such as IoT-, cloud- and software-security.',
+      companyTwo: 'About Q2d Solutions AB',
+      companyTwoInfo: 'Q2d Solutions AB develops solutions for IoT security life cycle management. This includes real time monitoring of wireless sensor networks, management of security and integration to companies existing systems.',
+      contact: 'Contact person',
+      contactPerson: 'Ludwig Seitz, RISE SICS AB, Security Lab (ludwig.seitz@ri.se)'
+    }
+  }]
+}
+})
